b1821c45b018a6049753798db40a42e8333f5438
Cristalvaldez07 <vzcristal@gmail.coml>
2021-05-18 18:34:41 -0400

    Trabajando con index JS

---
+++ sequential/index.js
@@ -0,0 +1,21 @@
+const { taskOne, taskTwo } = require('./tasks');
+
+
+async function main() {
+  try {
+    console.time('Measuring time');
+    const results = await Promise.all ([taskOne(), taskTwo()])
+
+    console.timeEnd('Measuring time'); 
+
+    console.log('Task One returned: ', valueOne[0]);
+    console.log('Task Two returned: ', valueTwo[1]);
+    
+  
+  }
+  catch (e) {
+    console.log(e); 
+  }
+}
+
+main();
